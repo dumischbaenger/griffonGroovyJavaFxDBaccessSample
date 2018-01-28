2b508f7ab115b7978c5693f39ceaf0092ae6c95c
bdonauba <bdonauba@brix.fritz.box>
2018-01-28 18:05:07 +0100

    m

--- src/main/groovy/de/dumischbaenger/ApplicationEventHandler.groovy
+++ src/main/groovy/de/dumischbaenger/ApplicationEventHandler.groovy
@@ -1,5 +1,6 @@
 package de.dumischbaenger
 
+import griffon.core.GriffonApplication
 import griffon.core.event.EventHandler
 import griffon.exceptions.GriffonViewInitializationException
 import javafx.application.Platform
@@ -8,4 +9,10 @@ class ApplicationEventHandler implements EventHandler {
     void onUncaughtGriffonViewInitializationException(GriffonViewInitializationException x) {
         Platform.exit()
     }
+	
+	void onBootstrapStart(GriffonApplication application) {
+		println("I am here")
+		println application.startupArgs
+	}
+
 }
