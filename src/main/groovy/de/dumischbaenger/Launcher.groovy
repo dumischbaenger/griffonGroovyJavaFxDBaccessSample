2b48278657968cf826c9a76e3732b7336c968d7a
bd <bd@brix.fritz.box>
2018-02-15 19:56:15 +0100

    tell hibernate to use slf4j

--- src/main/groovy/de/dumischbaenger/Launcher.groovy
+++ src/main/groovy/de/dumischbaenger/Launcher.groovy
@@ -3,7 +3,12 @@ package de.dumischbaenger
 import griffon.javafx.JavaFXGriffonApplication
 
 public class Launcher {
-    public static void main(String[] args) throws Exception {
-        JavaFXGriffonApplication.main(args)
-    }
+  static {
+    //BD Tell Hibernate to use SLF4J
+    //BD https://stackoverflow.com/questions/11639997/how-do-you-configure-logging-in-hibernate-4-to-use-slf4j
+    System.setProperty("org.jboss.logging.provider", "slf4j");
+  }
+  public static void main(String[] args) throws Exception {
+    JavaFXGriffonApplication.main(args)
+  }
 }
