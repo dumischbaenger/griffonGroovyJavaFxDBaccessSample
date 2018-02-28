e0a16026302a49fbf6fb5504758c4b53d0da3957
bdonauba <bdonauba@gmail.com>
2018-02-28 07:19:43 +0100

    JPA test and trial routine

---
+++ src/main/groovy/de/dumischbaenger/JpaPlayGround.groovy
@@ -0,0 +1,35 @@
+package de.dumischbaenger
+
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
+
+import de.dumischbaenger.domainmodel.Person
+
+class JpaPlayGround {
+
+  static main(args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("exampledb");
+    EntityManager em = emf.createEntityManager();
+
+    em.getTransaction().begin();
+    
+    Person p1 = new Person();
+    p1.age=13
+    p1.name="p1"
+    p1.gender=1
+    em.persist(p1);
+
+    Person p2 = new Person();
+    p2.age=13
+    p2.name="p2"
+    p2.gender=1
+    em.persist(p2);
+
+    em.getTransaction().commit();
+
+    emf.close();
+    
+  }
+
+}
